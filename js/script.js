2caa01e9dc59e6288d4ea7613da36e0da3e92f5b
David Bischof <bischof.david.db@gmail.com>
2023-02-03 16:34:29 +0100

    Finale

--- js/script.js
+++ js/script.js
@@ -1,54 +1,55 @@
 
-function getData(){
+function getData() {
   fetch("http://localhost:3000/tasks")
-  .then((response) => response.json())
-  .then(json => {
-  taskarray = json
-  render(taskarray)
-})
+    .then((response) => response.json())
+    .then(json => {
+      taskarray = json
+      render(taskarray)
+    })
 }
+
 getData();
 
- //Here are the Functions to load and edit the array of tasks
+//Here are the Functions to load and edit the array of tasks
 let taskarray = [];
-function render(){
+function render() {
   document.getElementById("tasks").replaceChildren()
-  taskarray.forEach(function(element, i) {
+  taskarray.forEach(function (element, i) {
 
 
     let newTask = document.createElement("div");
     newTask.innerText = element.title ?? element
     newTask.classList.add("todotask");
     newTask.setAttribute("onclick", "edittask(event)")
-  
-    
-  let deletebtn = document.createElement("button")
-  deletebtn.innerText = "Delete"
-  document.getElementById("tasks").appendChild(deletebtn);
 
-  deletebtn.classList.add("todotask");
-  deletebtn.addEventListener('click', function() {  // Delete and Abgabe button with Sanju
 
-    
-    taskarray.splice(i, 1)
-    render()
-    alert("Task gelöscht")
+    let deletebtn = document.createElement("button")
+    deletebtn.innerText = "Delete"
+    document.getElementById("tasks").appendChild(deletebtn);
 
-    
-  });
-  let donebtn = document.createElement("button")
-  donebtn.innerText = "Abgabe"
-  document.getElementById("tasks").appendChild(donebtn);
+    deletebtn.classList.add("todotask");
+    deletebtn.addEventListener('click', function () {  // Delete and Abgabe button with Sanju
 
-  donebtn.classList.add("todotask");
-  donebtn.addEventListener('click', function() {
 
-    
-    taskarray.splice(i, 1)
+      taskarray.splice(i, 1)
+      render()
+      alert("Task gelöscht")
 
-    render()
-    alert("Task abgeschlossen")
-  });
+
+    });
+    let donebtn = document.createElement("button")
+    donebtn.innerText = "Abgabe"
+    document.getElementById("tasks").appendChild(donebtn);
+
+    donebtn.classList.add("todotask");
+    donebtn.addEventListener('click', function () {
+
+
+      taskarray.splice(i, 1)
+
+      render()
+      alert("Task abgeschlossen")
+    });
 
 
 
@@ -66,15 +67,15 @@ render(); // So that the data get put into the list on first load
 
 let target;
 let edited;
-function edittask(event){
+function edittask(event) {
   target = event.currentTarget
   id = target.dataset.id
   edited = prompt("Hier kannst du deine Task bearbeiten", target.innerText)
-  if(edited==="" || edited==null){
+  if (edited === "" || edited == null) {
     alert("Zum Bearbeiten etwas eigeben")
     return
   }
-  
+
   target.innerText = edited
   taskarray[id] = edited;
 }
@@ -84,29 +85,46 @@ function edittask(event){
 const submitButton = document.querySelector('#submit');
 let entryText;
 const tasksContainer = document.getElementById('Tasks');
-let singletask; 
+let singletask;
 
 
 // Creates a new Task
 
-function enterkey(){
-  if(event.keyCode == 13) {
+function enterkey() {
+  if (event.keyCode == 13) {
 
-  entry = document.getElementById("entryText")
-  entryText = document.getElementById("entrytext").value;
-      if(entryText == ""){
-  
+    entry = document.getElementById("entryText")
+    entryText = document.getElementById("entrytext").value;
+    if (entryText == "") {
+
       alert("Bitte etwas in der task eingeben");
       exit
-  }
+    }
+    function postData() {
+      fetch("http://localhost:3000/auth/cookie/tasks", {
+        credentials: "include",
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ title: taskarray })
+      })
+        .then(response => {
+          if (response.ok) {
+            render(taskarray)
+          } else {
+            alert("Failed")
+          }
+        })
+        render();
+    }
     const newTask = document.createElement('div');
     newTask.classList.add("todotask");
     newTask.setAttribute("onclick", "editor(event)")
-    newTask.innerText= `${entryText}`;
+    newTask.innerText = `${entryText}`;
     document.getElementById("tasks").appendChild(newTask);
-  
+
     taskarray.push(entryText);
+    postData();
     document.getElementById("entrytext").value = null;
     render();
-}
+  }
 }
