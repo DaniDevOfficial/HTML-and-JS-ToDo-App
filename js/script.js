2dbca3240edc680fab3a25df6b60ed35387201dc
David Bischof <bischof.david.db@gmail.com>
2023-02-03 13:39:47 +0100

    Alle Buttons funktionieren

--- js/script.js
+++ js/script.js
@@ -1,7 +1,77 @@
 
 
+// Here are the Functions to load and edit the array of tasks
+let taskarray = ["snj", "Tasks"];
+function render(){
+  document.getElementById("tasks").replaceChildren()
+  taskarray.forEach(function(element, i) {
 
-let taskarray = [];
+
+    let newTask = document.createElement("div");
+    let textnode = document.createTextNode(element);
+    newTask.appendChild(textnode);
+    newTask.classList.add("todotask");
+    newTask.setAttribute("onclick", "edittask(event)")
+  
+    
+  let deletebtn = document.createElement("button")
+  deletebtn.innerText = "Delete"
+  document.getElementById("tasks").appendChild(deletebtn);
+
+  deletebtn.classList.add("todotask");
+  deletebtn.addEventListener('click', function() {
+
+    
+    taskarray.splice(i, 1)
+    render()
+    alert("Task gelöscht")
+
+    
+  });
+  deletebtn = document.createElement("button")
+  deletebtn.innerText = "Abgabe"
+  document.getElementById("tasks").appendChild(deletebtn);
+
+  deletebtn.classList.add("todotask");
+  deletebtn.addEventListener('click', function() {
+
+    
+    taskarray.splice(i, 1)
+
+    render()
+    alert("Task abgeschlossen")
+  });
+
+
+
+
+
+
+    document.getElementById("tasks").appendChild(newTask);
+  });
+}
+
+render(); // So that the data get put into the list on first load
+
+
+
+
+let target;
+let edited;
+function edittask(event){
+  target = event.currentTarget
+  id = target.dataset.id
+  edited = prompt("Hier kannst du deine Task bearbeiten", target.innerText)
+  if(edited==="" || edited==null){
+    alert("Zum Bearbeiten etwas eigeben")
+    return
+  }
+  
+  target.innerText = edited
+  taskarray[id] = edited;
+}
+
+
 
 const submitButton = document.querySelector('#submit');
 let entryText;
@@ -46,18 +116,9 @@ function enterkey(){
   
     taskarray.push(entryText);
     document.getElementById("entrytext").value = null;
+    render();
 }
 }
-let target;
-let edited;
-function editor(event){
-  target = event.currentTarget
-  console.log("it works");
-  edited = prompt("Hier kannst du deine Task bearbeiten", target.innerText)
-  if(edited==="" && edited==null){
 
-    exit
-  }
-  else{
-  target.innerText = `${edited}`
-  }}
+
+
